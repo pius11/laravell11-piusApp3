341dd3660c196a65c504a52d10eabd81028f2520
pius11 <pius17431@gmail.com>
2024-11-06 09:10:13 +0800

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>halaman about</title>
+</head>
+<body>
+    <a href="/">home</a>
+    <a href="/about">about</a>
+    <h1>halaman about</h1>
+    <h2>nama = {{ $nama }}</h2>
+    <img src="img/Virtual.png" alt="" width="200px">
+
+    <script src="js/scrip.js"></script>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>halaman home</title>
+</head>
+<body>
+    <a href="/">home</a>
+    <a href="/about">about</a>
+    <h3>halaman home</h3>
+
+    <script src="js/scrip.js"></script>
+</body>
+</html>
